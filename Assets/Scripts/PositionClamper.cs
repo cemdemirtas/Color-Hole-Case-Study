ff8bae54fc3cffa21c06c06f5800a07f1bab5b48
Cem Demirtas <97852548+cemdemirtas@users.noreply.github.com>
2022-06-09 00:51:06 +0300

    Assets & basic mechanics have ok.

---
+++ Assets/Scripts/PositionClamper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PositionClamper : MonoBehaviour
+{
+
+    [SerializeField] private Vector2 xClamp;
+    [SerializeField] private Vector2 zClamp;
+
+    private void Awake()
+    {
+        xClamp = new Vector2(-1.77f,1.77f);
+        zClamp = new Vector2(-2.5f,29.8f);
+    }
+
+    private void Update()
+    {
+        var pos = transform.position;
+        pos.x = Mathf.Clamp(transform.position.x, xClamp.x, xClamp.y);
+        transform.position = pos;
+
+        pos = transform.position;
+        pos.z = Mathf.Clamp(transform.position.z, zClamp.x, zClamp.y);
+        transform.position = pos;
+    }
+
+    public void SetClampValueX(Vector2 xClamp)
+    {
+        this.xClamp = xClamp;
+    }
+
+    public void SetClampValueZ(Vector2 zClamp)
+    {
+        this.zClamp = zClamp;
+    }
+}
